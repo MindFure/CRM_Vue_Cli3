e0d71bbb8b9557f0224daf21f03a9d1fcbf4f429
Dmitriy Yuzov <69352692+MindFure@users.noreply.github.com>
2020-09-11 14:35:46 +0700

    Добавление Логин и Категории

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <component :is="layout"> 
+      <router-view />
+    </component>
+  </div>
+</template>
+
+
+<script>
+
+import Emptylayout from '@/layouts/emptylayout'
+import Mainlayout from '@/layouts/mainlayout'
+
+export default {
+  computed: {
+    layout() {
+      
+      return (this.$route.meta.layout || 'empty') + '-layout';
+    }
+    
+  },
+  components:{
+    Emptylayout,
+    Mainlayout,
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+
+@import 'assets/index.css';
+</style>
